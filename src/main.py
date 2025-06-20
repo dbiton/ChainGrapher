174f2ffb02382f1b97502c06c4c6066db847aafd
Dvir David Biton <dbiton@gmail.com>
2025-06-21 01:15:33 +0300

    Improve generate_data

--- src/main.py
+++ src/main.py
@@ -4,13 +4,14 @@ import os
 from dotenv import load_dotenv
 from concurrent.futures import ProcessPoolExecutor
 import itertools
+from concurrent.futures._base import as_completed
 
 load_dotenv()
 
 from interfaces.iota_interface import IotaInterface
 from interfaces.eth_call_interface import EthCallInterface
 from interfaces.eth_prestate_interface import EthPerstateInterface
-from interfaces.sui_interface import SuiInterface
+from interfaces.sui_interface import SuiInterface, USER_KINDS
 from graph_metrics import get_graph_metrics
 from plotters import plot_data, plot_graph
 from savers import save_to_file
@@ -46,36 +47,38 @@ def agg_load_compressed_file(dirpath, limit, k):
 
 
 def generate_data(dirpath, output_path, limit=None):
-    data_generator = agg_load_compressed_file(dirpath, limit, 10)
+    data_generator = agg_load_compressed_file(dirpath, limit, 300)
     write_header = not os.path.exists(output_path)
-    max_pending = os.cpu_count()
+    max_pending = 32
+
     with open(output_path, mode="a", newline="") as file:
         with ProcessPoolExecutor() as pool:
-            futures = [
-                pool.submit(process_trace, *data) for data in islice(data_generator, max_pending)
-            ]
+            futures = {pool.submit(process_trace, *data): data for data in islice(data_generator, max_pending)}
             all_submitted = len(futures) < max_pending
             writer = csv.writer(file)
             i = 0
-            while len(futures) > 0:
-                future = futures[0]
-                futures = futures[1:]
-                result = future.result()
-                if result is not None:
-                    sorted_keys = sorted(result.keys())
-                    sorted_values = [result[k] for k in sorted_keys]
-                    if write_header:
-                        write_header = False
-                        writer.writerow(sorted_keys)
-                    writer.writerow(sorted_values)
-                    print(f"wrote result {i} to output csv")
-                    i += 1
-                if not all_submitted:
-                    try:
-                        next_data = next(data_generator)
-                        futures.append(pool.submit(process_trace, *next_data))
-                    except StopIteration:
-                        all_submitted = True
+            while futures:
+                for future in as_completed(futures):
+                    result = future.result()
+                    del futures[future]
+                    if result is not None:
+                        sorted_keys = sorted(result.keys())
+                        sorted_values = [result[k] for k in sorted_keys]
+                        if write_header:
+                            write_header = False
+                            writer.writerow(sorted_keys)
+                        writer.writerow(sorted_values)
+                        print(result)
+                        print(f"wrote result {i} to output csv")
+                        i += 1
+                    if not all_submitted:
+                        try:
+                            next_data = next(data_generator)
+                            new_future = pool.submit(process_trace, *next_data)
+                            futures[new_future] = next_data
+                        except StopIteration:
+                            all_submitted = True
+                    break  # Exit early to allow re-entering as_completed with updated futures
 
 
 def get_files(folder_path, extension):
@@ -83,7 +86,7 @@ def get_files(folder_path, extension):
 
 
 def main():
-    output_path = "metrics_agg1000.csv"
+    output_path = "metrics_sui_agg100.csv"
     dirpath = "E:\\sui"
     if os.path.exists(output_path):
         os.remove(output_path)
