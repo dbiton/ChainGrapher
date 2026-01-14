ee681ab310291309f2832191f9c1b674f5f769b9
Yaron Hay <41547784+yaronhb@users.noreply.github.com>
2026-01-14 17:24:25 +0200

    pool reusing

--- src/loaders.py
+++ src/loaders.py
@@ -11,7 +11,8 @@ import os
 import sys
 import zlib
 import h5py
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
 
 def uncompress_chunk(dset, i):
     chunk = dset[i]
@@ -22,14 +23,22 @@ def uncompress_chunk(dset, i):
     chunk = chunk.decode('ascii')
     chunk = json.loads(chunk)
     return chunk
+def uncompress_chunk_process(dset, i, processpool):
+    chunk = dset[i]
+    if len(chunk) == 0:
+        return []
+    chunk = bytes(chunk)
+    future = processpool.submit(uncompress_chunk_given, chunk)
+    chunk = future.result()
+    return chunk
 def uncompress_chunk_given(arg):
-    i, chunk = arg
+    chunk = arg
     if len(chunk) == 0:
         return []
     chunk = zlib.decompress(chunk)
     chunk = chunk.decode('ascii')
     chunk = json.loads(chunk)
-    return i, chunk
+    return chunk
 def read_chunk_given(arg):
     i, dset=arg
     return i, bytes(dset[i])
@@ -86,50 +95,89 @@ def uncompress_chunk_loading(i, filepath):
         chunk = chunk.decode('ascii')
         chunk = json.loads(chunk)
         return i, chunk
-def load_compressed_file_executor(filepath: str, pool,max_pending):
+def load_compressed_file_executor(filepath: str, limit):
     try:
         if os.path.exists(filepath+"_errors.txt"):
             os.remove(filepath+"_errors.txt")
         if os.path.exists(filepath):
-            with (h5pickle.File(filepath, 'r') as f_h5,
-                  ThreadPoolExecutor(max_workers=max_pending) as iopool,
-                    # (h5py.File(filepath, 'r') as f_h5,
-                  open(filepath+"_errors.txt", 'a') as f_err):
-                dset = f_h5['dataset']
+            with h5py.File(filepath, 'r') as f:
+                dset = f['dataset']
                 i_entry = 0
                 chunk_count = dset.shape[0]
-                reads = iopool.map(read_chunk_given,map(lambda x: (x, dset),range(chunk_count)))
-                futures = [
-                    # pool.submit(uncompress_chunk_loading, filepath, i_chunk_submitted)
-                    # pool.submit(uncompress_chunk, dset, i_chunk_submitted)
-                    pool.submit(uncompress_chunk_given, chunk)
-                    for chunk in itertools.islice(reads,min(max_pending, chunk_count))
-                ]
-                i_chunk_submitted = len(futures)
-                while len(futures) > 0:
-                    future = futures[0]
-                    futures = futures[1:]
-                    # entries = future.result()
-                    i_chunk,entries = future.result()
-                    if i_chunk_submitted < chunk_count:
-                        # futures.append(pool.submit(uncompress_chunk_loading, filepath, i_chunk_submitted))
-                        # futures.append(pool.submit(uncompress_chunk, dset, i_chunk_submitted))
-                        pool.submit(uncompress_chunk_given, next(reads))
-                        i_chunk_submitted += 1
-                    for (i,entry) in entries:
-                        i_entry += 1
-                        logger.info(f"loaded {i_entry} values from {filepath}")
-                        if "error" in entry:
-                            f_err.write(f"{i_entry}\t{entry['error']['message']}\n")
-                            continue
-                        yield [i, entry["result"]]
-
+                # max_pending = 2
+                max_pending = int(os.getenv("LOAD_MAXPENDING", 2))
+                with ThreadPoolExecutor(max_pending) as iopool, ProcessPoolExecutor(max_pending) as procpool:
+                    futures = [
+                        iopool.submit(uncompress_chunk_process, dset, i_chunk, procpool)
+                        for i_chunk in range(min(max_pending, chunk_count))
+                    ]
+                    i_chunk = len(futures)
+                    while len(futures) > 0:
+                        future = futures[0]
+                        futures = futures[1:]
+                        entries = future.result()
+                        for (i,entry) in entries:
+                            i_entry += 1
+                            logger.info(f"loaded {i_entry} values from {filepath}")
+                            if "error" in entry:
+                                with open(filepath+"_errors.txt", 'a') as f:
+                                    f.write(f"{i_entry}\t{entry['error']['message']}\n")
+                                continue
+                            yield [i, entry["result"]]
+                            if i_entry == limit:
+                                return
+                        if i_chunk < chunk_count:
+                            iopool.submit(uncompress_chunk_process, dset, i_chunk, procpool)
+                            i_chunk += 1
         else:
             logger.error("No traces file found.")
     except Exception as e:
         logger.error(f"Failed loading {filepath} due to {e}")
         print(repr(e))
         os._exit(1)
+    #     os._exit(1)    try:
+    #     if os.path.exists(filepath+"_errors.txt"):
+    #         os.remove(filepath+"_errors.txt")
+    #     if os.path.exists(filepath):
+    #         with (h5pickle.File(filepath, 'r') as f_h5,
+    #               ThreadPoolExecutor(max_workers=max_pending) as iopool,
+    #                 # (h5py.File(filepath, 'r') as f_h5,
+    #               open(filepath+"_errors.txt", 'a') as f_err):
+    #             dset = f_h5['dataset']
+    #             i_entry = 0
+    #             chunk_count = dset.shape[0]
+    #             reads = iopool.map(read_chunk_given,map(lambda x: (x, dset),range(chunk_count)))
+    #             futures = [
+    #                 # pool.submit(uncompress_chunk_loading, filepath, i_chunk_submitted)
+    #                 # pool.submit(uncompress_chunk, dset, i_chunk_submitted)
+    #                 pool.submit(uncompress_chunk_given, chunk)
+    #                 for chunk in itertools.islice(reads,min(max_pending, chunk_count))
+    #             ]
+    #             i_chunk_submitted = len(futures)
+    #             while len(futures) > 0:
+    #                 future = futures[0]
+    #                 futures = futures[1:]
+    #                 # entries = future.result()
+    #                 i_chunk,entries = future.result()
+    #                 if i_chunk_submitted < chunk_count:
+    #                     # futures.append(pool.submit(uncompress_chunk_loading, filepath, i_chunk_submitted))
+    #                     # futures.append(pool.submit(uncompress_chunk, dset, i_chunk_submitted))
+    #                     pool.submit(uncompress_chunk_given, next(reads))
+    #                     i_chunk_submitted += 1
+    #                 for (i,entry) in entries:
+    #                     i_entry += 1
+    #                     logger.info(f"loaded {i_entry} values from {filepath}")
+    #                     if "error" in entry:
+    #                         f_err.write(f"{i_entry}\t{entry['error']['message']}\n")
+    #                         continue
+    #                     yield [i, entry["result"]]
+    #
+    #     else:
+    #         logger.error("No traces file found.")
+    # except Exception as e:
+    #     logger.error(f"Failed loading {filepath} due to {e}")
+    #     print(repr(e))
+    #     os._exit(1)
 
 def load_file(filepath: str, limit=None):
     if os.path.exists(filepath):
